4bfaaa17de9bd8e1300b9df95eff5fe922c872cc
khafrewatkins <khafrewatkins@gmail.com>
2021-03-13 15:06:21 -0600

    1

---
+++ removefirstandlast.js
@@ -0,0 +1,9 @@
+function removeChar(str){
+ //You got this!
+  let str1 = str.slice(1)
+  let str2 = str1.slice(0, -1)
+  return str2
+//    let newstr = str.substring(1)
+//   let str1 = 
+//   return str1
+  }
