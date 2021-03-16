75634221f2baf444a0869538e7b9456451dd7843
khafrewatkins <khafrewatkins@gmail.com>
2021-03-16 14:47:53 -0500

    multiples

---
+++ multiplesOf.js
@@ -0,0 +1,12 @@
+function solution(number){
+  var total = 0;
+    for (var i = 0; i < number; i++)
+    {
+        if (i % 3 === 0 || i % 5 === 0)
+        {
+            total += i;
+        }
+    }
+    return total;
+    
+}
